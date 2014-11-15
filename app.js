d3d429ca4ace1b340b7180901441373f5203c81f
Chris Buttery <info@chrisbuttery.com>
2014-11-15 19:03:16 +1100

    Replaced duo-myth with myth

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var join = require('path').join;
 var write = require('fs').writeFileSync;
 
 var uglify = require('uglify-js');
-var myth = require('duo-myth');
+var myth = require('myth');
 var CleanCSS = require('clean-css');
 var cleancss = new CleanCSS();
 var Batch = require('batch');
@@ -20,7 +20,7 @@ var jsx = require('duo-jsx');
  */
 
 var dev = process.env.NODE_ENV === 'development';
-var src = __dirname+'/lib/boot/'
+var src = __dirname+'/lib/boot/';
 var root = process.cwd();
 var dest = './build/';
 var out = join(root, dest);
@@ -45,7 +45,6 @@ var js = Duo(__dirname)
 var css = Duo(__dirname)
   .entry(src + 'index.css')
   .buildTo(dest)
-  .use(myth())
   .copy(!dev);
 
 
@@ -53,7 +52,7 @@ var css = Duo(__dirname)
  * Run JS/CSS together
  */
 
-var batch = new Batch;
+var batch = new Batch();
 
 batch.push(js.run.bind(js));
 batch.push(css.run.bind(css));
@@ -61,9 +60,9 @@ batch.push(css.run.bind(css));
 batch.end(function(err, res){
   if (err) throw err;
 
-  var js = (dev) ? res.shift() : uglify.minify(res.shift(), { fromString: true}).code;
+  var js = uglify.minify(res.shift(), { fromString: true }).code;
   write(join(out, 'build.js'), js);
 
-  var css = (dev) ? res.shift() : cleancss.minify(res.shift());
+  var css = cleancss.minify(myth(res.shift()));
   write(join(out, 'build.css'), css);
 });
