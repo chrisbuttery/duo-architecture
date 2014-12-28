08b4f05c63f8761d5126bdb257e141954ec1c69f
Chris Buttery <info@chrisbuttery.com>
2014-12-28 21:48:23 +1100

    Remove some demo tests

--- test/rnd.js
+++
@@ -1,9 +0,0 @@
-
-var assert = require('component/assert');
-var rnd = require('../lib/rnd');
-
-describe('rnd', function(){
-  it('should return a number', function(){
-    assert('number' == typeof rnd());
-  })
-});
--- test/subtract.js
+++
@@ -1,8 +0,0 @@
-var assert = require('component/assert');
-var subtract = require('../lib/subtract');
-
-describe('subtract', function() {
- it('should subtract 4 - 2 to equal 2', function() {
-   assert.equal(subtract(4, 2), 2);
- });
-});
